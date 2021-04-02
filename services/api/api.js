677ff16dab8cc0e4bbe5042b01a6a692d7a6baa2
ruipedrodias94 <dias94.rui.pedro@gmail.com>
2021-04-02 19:03:41 +0100

    changed user in git

--- services/api/api.js
+++ services/api/api.js
@@ -5,6 +5,7 @@ const { port, env } = require("./config/variables");
 const mongoose = require("./config/mongoose");
 
 mongoose.connect();
+console.log("oi");
 
 app.listen(port, () => logger.info(`Server started on port ${port} (${env})`));
 
