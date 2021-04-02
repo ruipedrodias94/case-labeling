f82ce58e868a1171cd36e2e1b566f329ccbdf2bd
Rui Pedro Dias <rui.dias@bitcliq.com>
2021-04-02 18:44:58 +0100

    api main files structure

---
+++ services/api/api.js
@@ -0,0 +1,15 @@
+const logger = require("./config/logger");
+const app = require("./config/express");
+const { port, env } = require("./config/variables");
+
+const mongoose = require("./config/mongoose");
+
+mongoose.connect();
+
+app.listen(port, () => logger.info(`Server started on port ${port} (${env})`));
+
+/**
+ * Exports express
+ * @public
+ */
+module.exports = app;
